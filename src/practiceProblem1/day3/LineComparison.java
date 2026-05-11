e7ee1280cc6906d1aa93657963928b82e2279960
Prabhakaran Jayapal <prabhakaranjayapal26@gmail.com>
2026-05-11 09:27:12 +0530

    Welcome message added in Line Comparison Problem

--- src/practiceProblem1/day3/LineComparison.java
+++ src/practiceProblem1/day3/LineComparison.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to Line Comparison Computation Program");
         int x1=sc.nextInt();
         int y1=sc.nextInt();
         int x2=sc.nextInt();

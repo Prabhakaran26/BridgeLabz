ad9de3a37e7867ab2743c74659363150ec3d470d
Prabhakaran26 <prabhakaranjayapal26@gmail.com>
2026-05-21 14:38:50 +0530

    Add : [Prabhakaran] - Added day 13 Program

---
+++ src/practiceProblem1/day13/MinAndMax.java
@@ -0,0 +1,31 @@
+package practiceProblem1.day13;
+
+public class MinAndMax{
+    public static void main(String[] args) {
+        Integer[] intArr = {10, 20, 30, 40, 50};
+        Float[] floatArr = {11.2f, 54.3f, 8.7f, 99.1f};
+        String[] stringArr = {"Apple", "Peach", "Banana", "Zoo"};
+        System.out.print("Maximum Integer: ");
+        MaximumTest.testMaximum(intArr);
+        System.out.print("Maximum Float: " );
+        MaximumTest.testMaximum(floatArr);
+        System.out.print("Maximum String: ");
+        MaximumTest.testMaximum(stringArr);
+    }
+}
+class MaximumTest<T extends Comparable<T>>{
+
+    public static <T extends Comparable<T>> void testMaximum(T[] values){
+        T greatest = values[0];
+
+        for (T value : values) {
+            if (value.compareTo(greatest) > 0) {
+                greatest = value;
+            }
+        }
+        printGreatest(greatest);
+    }
+    public static <T> void printGreatest(T greatest){
+        System.out.println(greatest);
+    }
+}

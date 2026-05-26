a54f09e9b3d881c84765949c8c4633bef6e8c08f
Prabhakaran26 <prabhakaranjayapal26@gmail.com>
2026-05-26 10:12:46 +0530

    Add : [Prabhakaran] - Day 8 Problems Added

---
+++ src/practiceProblem1/day8/Employee.java
@@ -0,0 +1,65 @@
+package practiceProblem1.day8;
+
+import java.util.Random;
+
+public class Employee{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        String company=args[0];
+        int workingHours=Integer.parseInt(args[1]);
+        int workingDays=Integer.parseInt(args[2]);
+        int dailyWages=Integer.parseInt(args[3]);
+        int partTimeHours=Integer.parseInt(args[4]);
+        int fullTimeHours=Integer.parseInt(args[5]);
+        EmployeeWagesCalculation empWages=new EmployeeWagesCalculation(company,workingHours,workingDays,dailyWages,partTimeHours,fullTimeHours);
+        System.out.println(empWages.getMonthlySalarr());
+    }
+}
+class EmployeeWagesCalculation{
+    public static Random random=new Random();
+    public static int isFullTime=1;
+    public static int isPartTime=2;
+    private String company;
+    private int workingHours;
+    private int workingDays;
+    private int dailyWages;
+    private int partTimeHours;
+    private int fullTimeHours;
+    public EmployeeWagesCalculation(String company,int workingHours,int workingDays,int dailyWages,int partTimeHours,int fullTimeHours){
+        this.company=company;
+        this.workingHours=workingHours;
+        this.workingDays=workingDays;
+        this.dailyWages=dailyWages;
+        this.partTimeHours=partTimeHours;
+        this.fullTimeHours=fullTimeHours;
+    }
+    public int getMonthlySalarr(){
+        int monthlySalary=0;
+        int worked_days=0;
+        int worked_hours=0;
+        while(worked_days<workingDays && worked_hours<workingHours){
+            int attendence=random.nextInt(2);
+            if(attendence==1){
+                int workingType=random.nextInt(2)+1;
+                switch (workingType) {
+                    case 1:
+                        monthlySalary=monthlySalary+fullTimeHours*dailyWages;
+                        worked_hours=worked_hours+fullTimeHours;
+                        break;
+
+                    case 2:
+                        monthlySalary=monthlySalary+partTimeHours*dailyWages;
+                        worked_hours=worked_hours+partTimeHours;
+                        break;
+                    default :
+                        break;
+                }
+            }
+            else{
+                monthlySalary=monthlySalary+0;
+            }
+            worked_days++;
+        }
+        return monthlySalary;
+    }
+}
---
+++ src/practiceProblem1/day8/LineComparision.java
@@ -0,0 +1,58 @@
+package practiceProblem1.day8;
+
+import java.util.Scanner;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparision program on main");
+        Scanner sc=new Scanner(System.in);
+        int x1=sc.nextInt();
+        int y1=sc.nextInt();
+        int x2=sc.nextInt();
+        int y2=sc.nextInt();
+        int x3=sc.nextInt();
+        int y3=sc.nextInt();
+        int x4=sc.nextInt();
+        int y4=sc.nextInt();
+        CalculationLine cl1=new CalculationLine(x1, y1, x2, y2);
+        CalculationLine cl2=new CalculationLine(x3, y3, x4, y4);
+
+        double lineOneHeight=cl1.lineHeight();
+        double lineTwoHeight=cl2.lineHeight();
+        System.out.println("The Height of Line one is : "+lineOneHeight);
+        System.out.println("The height of second line is : "+lineTwoHeight);
+        if(Double.valueOf(lineOneHeight).equals(Double.valueOf(lineTwoHeight))){
+            System.out.println("Both Lines Are Equal");
+        }
+        else{
+            System.out.println("Both Lines Are Not Same");
+        }
+        int value=Double.valueOf(lineOneHeight).compareTo(Double.valueOf(lineTwoHeight));
+        if(value>0){
+            System.out.println("Line one is bigger");
+        }
+        else if(value==0){
+            System.out.println("Both lines are equal");
+        }
+        else{
+            System.out.println("Line two is IS bigger");
+        }
+    }
+}
+class CalculationLine{
+    private int x1;
+    private   int y1;
+    private  int x2;
+    private  int y2;
+    CalculationLine(int x1,int y1,int x2,int y2){
+        this.x1=x1;
+        this.y1=y1;
+        this.x2=x2;
+        this.y2=y2;
+    }
+    public double lineHeight(){
+        return Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+    }
+
+}
+

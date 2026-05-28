2a94da66229f13fc4fc98501408b7c875cfdc531
Prabhakaran26 <prabhakaranjayapal26@gmail.com>
2026-05-28 16:24:21 +0530

    Create : [Prabhakaran] - Created Main Class

---
+++ src/practiceProblem1/day22/AddressBookSystem.java
@@ -0,0 +1,8 @@
+package practiceProblem1.day22;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Wecome to Address Book System");
+    }
+
+}

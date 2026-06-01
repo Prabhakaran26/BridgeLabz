516f6d1828e5930c7dfcfbd25696f28a2e12b6d2
Prabhakaran26 <prabhakaranjayapal26@gmail.com>
2026-06-01 11:04:07 +0530

    Add : [Prabhakaran] - Added day 15Problems

---
+++ src/practiceProblem1/day15/BST.java
@@ -0,0 +1,124 @@
+package practiceProblem1.day15;
+
+public class BST {
+    public static void main(String[] args) {
+
+        BinaryTree<Integer> tree = new BinaryTree<>();
+
+        Integer[] array = {56, 30, 20, 40, 11, 3, 16, 70, 60, 95, 65, 63, 67};
+
+        for (Integer ele : array) {
+            tree.insert(ele);
+        }
+
+        System.out.println("Tree Has");
+        tree.print();
+
+        System.out.println();
+
+        if (tree.size() == array.length) {
+            System.out.println("Every element is inserted in the tree");
+        } else {
+            System.out.println("Not Inserted Completely");
+        }
+
+        if (tree.search(63)) {
+            System.out.println("Node is Present");
+        } else {
+            System.out.println("Node is not present");
+        }
+    }
+}
+
+class Node<T> {
+
+    T data;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T data) {
+        this.data = data;
+    }
+}
+
+class BinaryTree<T extends Comparable<T>> {
+
+    Node<T> root;
+
+    public void insert(T data) {
+        root = insertRec(root, data);
+    }
+
+    public Node<T> insertRec(Node<T> root, T data) {
+
+        if (root == null) {
+            root = new Node<>(data);
+        }
+
+        else if (data.compareTo(root.data) < 0) {
+            root.left = insertRec(root.left, data);
+        }
+
+        else if (data.compareTo(root.data) > 0) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void print() {
+        printRec(root);
+    }
+
+    public void printRec(Node<T> root) {
+
+        if (root != null) {
+
+            printRec(root.left);
+
+            System.out.print(root.data + " ");
+
+            printRec(root.right);
+        }
+    }
+
+    public int size() {
+        return sizeOfTree(root);
+    }
+
+    public int sizeOfTree(Node<T> root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int s1 = sizeOfTree(root.left);
+        int s2 = sizeOfTree(root.right);
+
+        return s1 + s2 + 1;
+    }
+
+    public boolean search(T data) {
+        return searchTree(data, root);
+    }
+
+    public boolean searchTree(T data, Node<T> root) {
+
+        if (root != null) {
+
+            if (data.compareTo(root.data) == 0) {
+                return true;
+            }
+
+            else if (data.compareTo(root.data) < 0) {
+                return searchTree(data, root.left);
+            }
+
+            else if (data.compareTo(root.data) > 0) {
+                return searchTree(data, root.right);
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/practiceProblem1/day15/HashProgram.java
@@ -0,0 +1,117 @@
+package practiceProblem1.day15;
+
+public class HashProgram {
+    public static void main(String[] args) {
+        String sentence= "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words=sentence.toLowerCase().split(" ");
+        MyHashMap<String,Integer> map=new MyHashMap<>();
+        for(String word:words){
+            Integer count=map.get(word);
+            if(count==null){
+                map.put(word,1);
+            }
+            else{
+                map.put(word,count+1);
+            }
+        }
+        System.out.println("Before Removing");
+        map.print();
+        map.remove("avoidable");
+        System.out.println();
+        System.out.println("After Removing");
+        map.print();
+    }
+}
+class MyMapNode<K,V>{
+    K key;
+    V value;
+    MyMapNode<K,V> next;
+    MyMapNode(K key,V value){
+        this.key=key;
+        this.value=value;
+    }
+}
+class MyHashMap<K,V>{
+    MyMapNode<K,V>[] buckets;
+    MyHashMap(){
+        buckets=new MyMapNode[10];
+    }
+    public  int getIndex(K key) {
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+    public void put(K key,V value){
+        int index=getIndex(key);
+        MyMapNode<K,V> temp=buckets[index];
+        if(buckets[index]==null){
+
+            buckets[index]=new MyMapNode<>(key,value);
+            return;
+        }
+        while(temp!=null){
+
+            if(temp.key.equals(key)){
+                temp.value=value;
+                return;
+            }
+
+            if(temp.next==null){
+                break;
+            }
+
+            temp=temp.next;
+        }
+
+        temp.next=new MyMapNode<>(key,value);
+    }
+    public V get(K key){
+        int index=getIndex(key);
+        MyMapNode<K,V> temp=buckets[index];
+
+        while(temp!=null){
+
+            if(temp.key.equals(key)){
+                return temp.value;
+            }
+
+            temp=temp.next;
+        }
+
+        return null;
+    }
+    public void print(){
+        for(int i=0;i<buckets.length;i++){
+
+            MyMapNode<K,V> temp=buckets[i];
+
+            if(temp!=null){
+
+                System.out.println("Bucket "+i);
+            }
+
+            while(temp!=null){
+
+                System.out.println(temp.key+" -> "+temp.value);
+
+                temp=temp.next;
+            }
+        }
+    }
+    public void remove(K key){
+        int index=getIndex(key);
+        MyMapNode<K,V> temp=buckets[index];
+        MyMapNode<K,V> prev=null;
+        while(temp!=null){
+            if(temp.key.equals(key)){
+                if(prev==null){
+                    buckets[index]=temp.next;
+                }
+                else{
+                    prev.next=temp.next;
+                }
+                return;
+            }
+            prev=temp;
+            temp=temp.next;
+        }
+    }
+}
